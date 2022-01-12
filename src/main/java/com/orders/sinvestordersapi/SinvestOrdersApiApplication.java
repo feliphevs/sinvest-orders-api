3f8c54d87f33b649645dea9ecdf9e41894aa3d0a
Feliphe Silva <feliphevs90@outlook.com>
2022-01-12 17:39:03 -0300

    commit inicial

---
+++ src/main/java/com/orders/sinvestordersapi/SinvestOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package com.orders.sinvestordersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinvestOrdersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinvestOrdersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orders/sinvestordersapi/SinvestOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders.sinvestordersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinvestOrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
