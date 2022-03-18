5b319f15cbd8c76474a84103796b74792da0af3a
Feliphe Silva <feliphevs90@outlook.com>
2022-03-18 18:06:30 -0300

    cross origin configurado

--- src/main/java/com/orders/sinvestordersapi/api/controller/UserController.java
+++ src/main/java/com/orders/sinvestordersapi/api/controller/UserController.java
@@ -16,6 +16,7 @@ import com.orders.sinvestordersapi.domain.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/users")
 public class UserController {
--- src/main/java/com/orders/sinvestordersapi/api/controller/UserOrderController.java
+++ src/main/java/com/orders/sinvestordersapi/api/controller/UserOrderController.java
@@ -14,6 +14,7 @@ import com.orders.sinvestordersapi.domain.service.UserOrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/userorders")
 public class UserOrderController {
